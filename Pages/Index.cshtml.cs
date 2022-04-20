2bbfbcf25953c227dac8526f4154d04b80cb1dd6
chris219219 <ctan0619@gmail.com>
2022-04-20 10:56:02 -0400

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EqSolve;
+
+namespace EqSolveWeb.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [BindProperty]
+    public string Equation { get; set; } = string.Empty;
+
+    [BindProperty]
+    public string X { get; set; } = "0";
+
+    [BindProperty]
+    public string Y { get; set; } = "0";
+
+    [BindProperty]
+    public string Z { get; set; } = "0";
+
+    [BindProperty]
+    public string Answer { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+        try
+        {
+            double answer = Solver.Solve(Parser.ParseToTerms(Equation, X, Y, Z));
+            Answer = answer.ToString();
+        }
+        catch (Exception)
+        {
+            Answer = "Invalid equation or xyz values.";
+        }
+    }
+}
